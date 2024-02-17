10b8d90019be79627ad0710f2f392c7bec14ff67
Abdelwahab <mramged936@gmail.com>
2024-02-17 11:18:56 +0200

    Strip payments

--- src/index.js
+++ src/index.js
@@ -11,13 +11,10 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// dev-ghwsn5qao4rum25k.us.auth0.com
-// vSwFUtEEyFDSJ7um2scJ7L2O3C9PRmvZ
-// F-nYj34YhFBrkT4kpscF1LEgyD1IvD1VidWksEJ5qXoqYF--QvvB8OECYxZp8BxR
 root.render(
   <Auth0Provider
-    domain="dev-ghwsn5qao4rum25k.us.auth0.com"
-    clientId="vSwFUtEEyFDSJ7um2scJ7L2O3C9PRmvZ"
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
     // authorizationParams={
     //   {
     //     redirect_uri: `/cart`,
